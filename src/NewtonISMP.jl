7989de83ca14e05a0d33982788295d52581a96d6
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2015-03-16 21:27:59 -0300

    Add test-cutest

--- src/NewtonISMP.jl
+++ src/NewtonISMP.jl
@@ -6,22 +6,16 @@ function solve(f, g, H, x0; eps=1e-6, kmax=1000, alpha=0.5, neg_thresh=1e-12)
   while norm(g(x)) > eps
     d = -H(x)\g(x);
     # Negative curvature or near singular
-    if -dot(g(x),d) < neg_thresh*dot(g(x),g(x))
-      return 1, x, f(x), g(x), k
-    end
+    -dot(g(x),d) < neg_thresh*dot(g(x),g(x)) && return 1, x, f(x), g(x), k
     # Line search
     t = 1.0
     while f(x+t*d) >= f(x) + alpha*t*dot(g(x),d)
       t *= 0.9;
-      if t < Base.eps()
-        return 3, x, f(x), g(x), k
-      end
+      t < Base.eps() && return 3, x, f(x), g(x), k
     end
     x += t*d;
     k += 1
-    if k >= kmax
-      return 2, x, f(x), g(x), k;
-    end
+    k >= kmax && return 2, x, f(x), g(x), k;
   end
   return 0, x, f(x), g(x), k;
 end
